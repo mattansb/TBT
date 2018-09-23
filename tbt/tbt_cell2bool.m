d26298162e9fb43a7f47fe431bb51aab600e7f61
mattansb <35330040+mattansb@users.noreply.github.com>
2018-09-23 15:27:54 +0300

    always convert bads to logical

--- tbt/tbt_cell2bool.m
+++ tbt/tbt_cell2bool.m
@@ -1,12 +1,12 @@
 function bads_array = tbt_cell2bool(bads,EEG)
 
-bads_array = zeros([size(EEG.data,1) size(EEG.data,3)]);
+bads_array = false([size(EEG.data,1) size(EEG.data,3)]);
 
 for r = 1:size(bads,1)
     % identift channels
     chan_i  = cellfun(@(x) any(strcmpi(x,bads{r,2})),{EEG.chanlocs.labels});
     epoch_i = bads{r,1};
-    bads_array(chan_i,epoch_i) = 1;
+    bads_array(chan_i,epoch_i) = true;
 end
 
 end
